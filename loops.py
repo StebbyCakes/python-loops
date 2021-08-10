d2d7c1d9e32e4aa78de1fe6a284d8b1b5761d00f
Esteban Carter <carterea1999@gmail.com>
2021-08-10 12:06:02 -0400

    printing all elements in an array & sum

---
+++ loops.py
@@ -0,0 +1,10 @@
+a = ["banana", "apple", "microsoft"]
+for element in a:
+    print(element);
+
+# sum of all elements in an array
+b = [20, 10 , 5]
+total = 0
+for i in b:
+    total = total + i
+print(total)
